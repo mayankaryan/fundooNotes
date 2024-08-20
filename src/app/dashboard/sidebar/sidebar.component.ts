87118a1fbd366f7ae9d0c11d30b5c1cf8b7a3340
Mayank Aryan <Mayank.Aryan@godigit.com>
2024-08-20 16:32:04 +0530

    ADDED: sidebar component on click css and functionality

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -9,6 +10,13 @@ import { UserService } from 'src/app/services/user/user.service';
 
 export class SidebarComponent {
   buttonClicked:boolean = false;
+  clc_note:boolean = false;
+  clc_reminder:boolean = false;
+  clc_label:boolean = false;
+  clc_edit:boolean = false;
+  clc_archive:boolean = false;
+  clc_trash:boolean = false;
+
 
   constructor(private userService: UserService) {}
 
@@ -20,6 +28,15 @@ export class SidebarComponent {
 }
 appTitle(title:string) {
   // console.log(title);
+  this.clc_archive = this.clc_edit = this.clc_label = this.clc_note = this.clc_reminder = this.clc_trash = false;
+  if(title == 'Notes') this.clc_note = true;
+  else if(title == 'Reminders') this.clc_reminder = true;
+  else if(title == 'Label') this.clc_label = true;
+  else if(title == 'Edit Label') this.clc_edit = true;
+  else if(title == 'Archive') this.clc_archive = true;
+  else if(title == 'Trash') this.clc_trash = true;
   this.userService.changeAppTitle(title);
 }
+
+
 }
